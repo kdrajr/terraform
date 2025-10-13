e519e98c4caf0c0dc1e2ad6f3e90c07059fef492
Dheeraj <31932986+kdrajr@users.noreply.github.com>
2025-10-13 11:49:15 +0530

    create ec2 instance with terraform

--- 01-ec2-instance.tf
+++ 01-ec2-instance.tf
@@ -1,11 +1,17 @@
 resource "aws_instance" "ansible" {
-  name = "ansible"
   ami           = "ami-09c813fb71547fc4f"
-  vpc_security_group_ids = "sg-0f01e6396107117bf"
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
   instance_type = "t3.micro"
 
   tags = {
-    Name = "terraform"
+    Name = "ansible"
+    terraform = true
   }
 }
 
+data "aws_security_group" "existing_sg" {
+  id = "sg-0f01e6396107117bf"
+}
+
+#security_groups = [data.aws_security_group.existing_sg.name]
+#name = "devsecgrp" 
