814da679032a305edd173f22000d70c71caad623
Dheeraj <31932986+kdrajr@users.noreply.github.com>
2025-10-21 16:09:21 +0530

    count-loop

---
+++ count-loop/r53.tf
@@ -0,0 +1,9 @@
+resource "aws_route53_record" "dns_name" {
+    count = 4
+    zone_id = var.zoneid
+    name    = var.instances[count.index] == "frontend" ? "${var.domain_name}" : "${var.instances[count.index]}.${var.domain_name}"
+    type    = "A"
+    ttl     = 1
+    allow_overwrite = true
+    records = var.instances[count.index] == "frontend" ? [aws_instance.roboshop[count.index].public_ip] : [aws_instance.roboshop[count.index].private_ip]
+}
---
+++ count-loop/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+  default = [ "mongodb","mysql","redis","frontend" ]
+}
+
+variable "zoneid" {
+  default = "Z0975378N0ET60VBIODO"
+}
+
+variable "domain_name" {
+  default = "sniggie.fun"
+}
